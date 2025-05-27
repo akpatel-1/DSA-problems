58e96878b2374fb446c7e2dbef3dd5f7323f7306
akpatel-1 <akpatel.work1@gmail.com>
2025-05-27 19:22:25 +0530

    Auto gitpush script file

---
+++ 2239. Find Closest Number to Zero.java
@@ -0,0 +1,21 @@
+/*
+Problem: 2239. Find Closest Number to Zero
+Link: https://leetcode.com/problems/find-closest-number-to-zero/description/
+ 
+Approach: Initialize closest=nums[0] -> loop num:nums -> check if(Math.abs(num)<Math.abs(closest) || Math.abs(num)==Math.abs(closest) && num>closest) -> return closest
+ 
+TC: O(n) single loop
+SC: O(1) primitive data used
+ */
+
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int closest = nums[0];
+        for(int num:nums){
+            if(Math.abs(num)<Math.abs(closest) || Math.abs(num)==Math.abs(closest) && num>closest){
+                closest = num;
+            }
+        }
+        return closest;
+    }
+}
--- gitpush.sh
+++ gitpush.sh
@@ -3,10 +3,8 @@
 # Stage all changes
 git add .
 
-# Get the list of added files
-files=$(git diff --cached --name-only)
-
-for file in $files; do
+# Get the list of added files (preserve spaces)
+git diff --cached --name-only -z | while IFS= read -r -d '' file; do
   # Extract the filename without extension
   base_name=$(basename "$file")
   name_without_ext="${base_name%.*}"
